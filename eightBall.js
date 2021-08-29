105e264b97201f3541c5f6f52e6e856af7068e7a
Caden Fore <cadenfore@gmail.com>
2021-08-29 03:34:15 -0500

    Add files via upload

---
+++ eightBall.js
@@ -0,0 +1,43 @@
+let userName = 'Jane';
+let userQuestion = 'Will I be an excellent programmer?';
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+
+
+//this checks for a username
+userName ? console.log(`Hello, ${userName}`) 
+: console.log('Hello!');
+
+//this prints the users question
+console.log(`The user ${userName} asked: ${userQuestion}`);
+
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain'
+    break;
+  case 1:
+    eightBall = 'It is decidedly so'
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again'
+    break;
+  case 3:
+    break;
+  case 4:
+    eightBall = 'Do not count on it'
+    break;
+  case 5:
+    eightBall = 'My sources say no'
+    break;
+  case 6:
+    eightBall = 'Outlook not so good'
+    break;
+  case 7:
+    eightBall = 'Signs point to yes'
+    break;
+};
+
+console.log(`The eightball answered: ${eightBall}`);
+
+
